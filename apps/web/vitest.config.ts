a03477d2a9c931fc5f961ca60da38274c67f03cf
Kunal Pisolkar <kpisolkar24@gmail.com>
2025-05-03 08:54:52 +0000

    performed vitestest setup

---
+++ apps/web/vitest.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/.next/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+    ],
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+      '@workspace/ui': path.resolve(__dirname, '../../packages/ui/src')
+    },
+  },
+});
