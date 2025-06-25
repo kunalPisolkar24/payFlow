2b18495fe05e59de4528901c66b87909e776e1b6
Kunal Pisolkar <kpisolkar24@gmail.com>
2025-06-25 05:47:06 +0000

    added cluster setup for kind

---
+++ setup-cluster.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -o errexit
+
+reg_name='kind-registry'
+reg_port='5001'
+if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
+  docker run \
+    -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    registry:2
+fi
+
+kind create cluster --config kind-cluster-config.yaml --name payflow-cluster
+
+docker network connect "kind" "${reg_name}"
+
+echo "Waiting for Ingress controller to be ready..."
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+echo "✅ Cluster 'payflow-cluster' is ready."
